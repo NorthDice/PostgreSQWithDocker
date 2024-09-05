09a27c7e4bf50526cf42477d26c02ce065a3be38
Balychev Kiril <mister.balychev@gmail.com>
2024-09-05 20:23:15 +0300

    Configuration for Permissions

---
+++ Configuration/PermissionConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using DotNet.Docker.Entities;
+using DotNet.Docker.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Data.Entity.ModelConfiguration;
+
+namespace DotNet.Docker.Configuration
+{
+    public class PermissionConfiguration : IEntityTypeConfiguration<PermissionEntity>
+    {
+        public void Configure(EntityTypeBuilder<PermissionEntity> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            var permission = Enum
+                .GetValues<Permission>()
+                .Select(p => new PermissionEntity
+                {
+                    Id = (int)p,
+                    Name = p.ToString()
+                });
+
+            builder.HasData(permission);
+        }
+    }
+}
